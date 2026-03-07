b78aef76bdcc3d96aa557324440e18ab9a8bfae4
Oluwajolasun Jaiyesimi <jolasunjaiyesimi@gmail.com>
2026-03-07 09:55:02 -0700

    Footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>DEVELOPED AND DESIGNED BY OLUWAJOLASUN 👨🏾‍💻. © 2026</p>
+      <p>DEVELOPED AND DESIGNED BY OLUWAJOLASUN 👨🏾‍💻 © 2026</p>
       <p
         style={{
           color: "var(--accent)",
